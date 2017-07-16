a7db918dd0980b9b770e494b3f21d20ff5e51a5c
glau-isidoro <glaucia.isidoro@gmail.com>
2017-07-16 20:43:22 -0300

    terceiro e quarto test suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,11 +27,10 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
          it('allFeeds have url not empty', function() {
              for(var i = 0 ; i < allFeeds.length ; i++) {
                  expect(allFeeds[i].url).toBeDefined();
@@ -41,11 +40,10 @@ $(function() {
          });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-
          it('allFeeds have name not empty', function() {
              for(var i = 0 ; i < allFeeds.length ; i++) {
                  expect(allFeeds[i].name).toBeDefined();
@@ -57,9 +55,9 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
-    describe('the menu', function() {
-        /* TODO: Write a test that ensures the menu element is
+    /* new test suite named "The menu" */
+    describe('The menu', function() {
+        /* test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
@@ -68,7 +66,7 @@ $(function() {
              expect($('body').hasClass('menu-hidden')).toBe(true);
          });
 
-         /* TODO: Write a test that ensures the menu changes
+         /* test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
@@ -82,19 +80,55 @@ $(function() {
 
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
-
-        /* TODO: Write a test that ensures when the loadFeed
+    /* new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
+        /* test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+         beforeEach(function(done) {
+             loadFeed(0, function() {
+                 done();
+             });
+         });
 
-        /* TODO: Write a test that ensures when a new feed is loaded
+         it('feed has element entry', function(done) {
+             expect($('.feed').has('.entry').length).toBeGreaterThan(0);
+             done();
+         });
+
+    });
+
+    /* new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
+        /* test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+
+         var firstTitle, otherTitle, firstEntry, otherEntry;
+
+         beforeEach(function(done) {
+             loadFeed(0, function() {
+                 firstTitle = $('.header-title').text();
+                 firstEntry = $('.feed a:first-of-type .entry h2').text();
+                 loadFeed(3, function() {
+                     done();
+                 });
+             });
+         });
+
+         it('ultimo teste', function(done) {
+             otherTitle = $('.header-title').text();
+             otherEntry = $('.feed a:first-of-type .entry h2').text();
+             expect(otherTitle).not.toBe(firstTitle);
+             expect(otherEntry).not.toBe(firstEntry);
+             done();
+         });
+
+    });
+
 }());
